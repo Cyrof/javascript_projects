8062256d06725fb190e81e404c02d1a99954e641
Cyrof <73649950+Cyrof@users.noreply.github.com>
2021-08-07 02:12:15 +0800

    updated

--- book_room/room_book.js
+++ book_room/room_book.js
@@ -10,6 +10,9 @@ function hideBookIn(){
     click[0].style.display = "none";
 }
 
+// set global count
+var check = "none";
+
 //function to show/hide calendar
 function showCalendar(num){
     var click = document.getElementsByClassName("calendar");
@@ -18,11 +21,17 @@ function showCalendar(num){
     }
     else{
         click[0].style.display = "block";
+        check = "block";
     }
 }
 
-window.addEventListener("click", check());
+// event listener function to listen for click outside calendar
+document.addEventListener("mouseup", function(event) {
+    var obj = document.getElementsByClassName("calendar");
+    var calendar = document.getElementsByClassName("calendar");
+    if(!obj[0].className.includes(event.target.className) && check === "block"){
+        calendar[0].style.display = "none";
+        check = "none";
+    } 
+});
 
-function check(){
-    alert("clicked!");
-}
