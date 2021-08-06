85f0f28e83b80f755300b08ff0bb70b1e99b768a
Cyrof <73649950+Cyrof@users.noreply.github.com>
2021-08-06 19:28:51 +0800

    update 06/08/2021 19:28

--- book_room/room_book.js
+++ book_room/room_book.js
@@ -4,3 +4,20 @@ function showBookIn(){
     click[0].style.display = "block";
 }
 
+//function to close book room page
+function hideBookIn(){
+    var click = document.getElementsByClassName("book-room-container");
+    click[0].style.display = "none";
+}
+
+//function to show/hide calendar
+function showCalendar(num){
+    var click = document.getElementsByClassName("calendar");
+    click.style.display = "block";
+    // if (num === 0){
+    //     click.style.display = "none";
+    // }
+    // else{
+    //     click.style.display = "block";
+    // }
+}
